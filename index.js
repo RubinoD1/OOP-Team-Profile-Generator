22f9178df4e7db63ed1e4f38807e7a2f5d072e4c
RubinoD1 <Rubinod1@gmail.com>
2022-07-10 21:18:16 -0400

    added manager questions and email/number checks

--- index.js
+++ index.js
@@ -13,5 +13,63 @@ const employeeArray = [];
 
 //The first employee to be added is the manager. Info is collected for this employee first. 
 const createManager = () => {
-    
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter manager's name",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                  } else { 
+                    console.log('A name must be entered for a manager');
+                    return false;
+                  }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter manager's id",
+            validate: nameInput => {
+                //cheecks for a number 
+                if (isNaN(nameInput)) {
+                    console.log('A id number must be entered');
+                    return false;
+                  } else { 
+                    return true;
+                  }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter manager's email address",
+            validate: email => {
+                //email valid format check
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                
+                if (valid) {
+                    return true;
+                  } else { 
+                    console.log('A email address must be entered');
+                    return false;
+                  }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter manager's office number",
+            validate: nameInput => {
+                //cheecks for a number 
+                if (isNaN(nameInput)) {
+                    console.log('A office number must be entered');
+                    return false;
+                  } else { 
+                    return true;
+                  }
+            } 
+        }
+    ])
 }
