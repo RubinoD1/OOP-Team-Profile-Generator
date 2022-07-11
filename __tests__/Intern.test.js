e8b7d2cc5f01a8a96f580dd3cd5295b7a0f4ca0b
RubinoD1 <Rubinod1@gmail.com>
2022-07-10 20:10:31 -0400

    intern tests in place all failing

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,23 @@
 //   ('Diane', 1, 'Fake@gmail.com', 'DianeGitHub');
 
 const Intern = require('../lib/Intern');
+
+ 
+test('creates an Intern object', () => {
+    const intern = new Intern('Diane', 1, 'Fake@gmail.com', 'Miskatonic University');
+    
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+
+test('gets employee school', () => {
+    const intern = new Intern('Diane', 1, 'Fake@gmail.com', 'Miskatonic University');
+    
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+
+test('gets role of employee', () => {
+    const intern = new Intern('Diane', 1, 'Fake@gmail.com', 'Miskatonic University');
+    expect(intern.getRole()).toEqual("Intern");
+}); 
