fe11edca7a060ce50acf70e21e77816fdc7420fd
RubinoD1 <Rubinod1@gmail.com>
2022-07-10 20:54:09 -0400

    started index.js added empty array and const for required files

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const inquirer = require('inquirer');
+const fs = require('fs'); 
+//HTML template
+const creeateHTML = require('./src/HTML-template.html');
 
+//Employee js files 
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+const Manager = require('./lib/Manager');
+
+//employee array- inquirer data will be pushed to this. 
+const employeeArray = [];
+
+//The first employee to be added is the manager. Info is collected for this employee first. 
+const createManager = () => {
+    
+}
